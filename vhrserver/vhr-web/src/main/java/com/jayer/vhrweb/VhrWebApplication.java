a2753f7f5e9134c16b60f73c1a33637b2c02214c
Jayer <jayer23@163.com>
2020-03-23 13:54:00 +0800

    项目构建

---
+++ vhrserver/vhr-web/src/main/java/com/jayer/vhrweb/VhrWebApplication.java
@@ -0,0 +1,13 @@
+package com.jayer.vhrweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrWebApplication.class, args);
+    }
+
+}
---
+++ vhrserver/vhr-web/src/test/java/com/jayer/vhrweb/VhrWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayer.vhrweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
